c15dacb28585d643eccacd56de4ff571662b27d7
Subham Pal <32848988+subhampal077@users.noreply.github.com>
2025-06-09 16:35:42 +0530

    Update ModalPopup.jsx

--- 15-React-34-Machine-Coding-Projects/1-React-17-Machine-Coding-Projects-Part1/src/components/11-Custom-Modal-PopUp/ModalPopup.jsx
+++ 15-React-34-Machine-Coding-Projects/1-React-17-Machine-Coding-Projects-Part1/src/components/11-Custom-Modal-PopUp/ModalPopup.jsx
@@ -3,7 +3,7 @@ import Modal from "./Modal";
 
 const ModalPopup = () => {
   const [showModal, setShowModal] = useState(false);
-
+  
   return (
     <div className="relative text-center ">
       <button
